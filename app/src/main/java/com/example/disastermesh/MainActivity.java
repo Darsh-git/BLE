c1adc789b6b9bb85ae7b002519af5ef3018cfa4e
keerthana-git11 <keerthanasenthilkumar30@gmail.com>
2026-09-12 09:55:19 +0000

    Updated code

--- app/src/main/java/com/example/disastermesh/MainActivity.java
+++ app/src/main/java/com/example/disastermesh/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.example.bleprototype.R;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
--- app/src/main/java/com/example/disastermesh/ReportAdapter.java
+++ app/src/main/java/com/example/disastermesh/ReportAdapter.java
@@ -6,6 +6,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.example.bleprototype.R;
 import java.util.List;
 
 public class ReportAdapter extends RecyclerView.Adapter<ReportAdapter.ReportViewHolder> {
